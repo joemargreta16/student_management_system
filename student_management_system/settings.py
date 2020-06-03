1eac0f36b8b8618eb73bf47ef514ae6e45e026c1
unknown <joemar.greta16@gmail.com>
2020-06-03 19:26:33 +0800

    Updated Setting

--- student_management_system/settings.py
+++ student_management_system/settings.py
@@ -150,6 +150,10 @@ EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_mails")
 # DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 import dj_database_url
+import django_heroku
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
 
 prod_db = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
